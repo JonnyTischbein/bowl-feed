df709d5fcaf225e2dfa0ccf30b8c6e3cf162f55a
Polynomialdivision <vincent@systemli.org>
2020-03-23 01:23:01 +0100

    luci-app-dawn: fix luci app

--- luci-app-dawn/luasrc/model/cbi/dawn/dawn_network.lua
+++ luci-app-dawn/luasrc/model/cbi/dawn/dawn_network.lua
@@ -27,46 +27,31 @@ function s.render(self, sid)
 				for mac, data in pairs(macs) do
 				%>
 					<li>
-						<strong>AP MAC is: </strong><%= mac %><br />
+						<strong>MAC: </strong><%= mac %><br />
+						<strong>Channel Utilization: </strong><%= "%d" %data.channel_utilization %><br />
+						<strong>Frequency is: </strong><%= "%.3f" %( data.freq / 1000 ) %> GHz (Channel: <%= "%d" %( status.frequency_to_channel(data.freq) ) %>)<br />
+						<strong>Stations: </strong><%= "%d" %data.num_sta %><br />
+						<strong>HT Sup: </strong><%= (data.ht_support == true) and "available" or "not available" %><br />
+						<strong>VHT Sup: </strong><%= (data.vht_support == true) and "available" or "not available" %><br />  
+						<strong>Clients: </strong><br />	
 					</li>
-					<ul>
-					<%
+                                        <% 					
 					local mac2, data2
-					for mac2, data2 in pairs(data) do					
-						local devicename
-						local host = mac2 and hosts[mac2]
-						local httpclient = require('luci.httpclient')
-						if not(data2.signature == nil) then
-							local key = "3f571d11466714f41d72bc9aac30858b80d36fa4"
-							local url = "http://wifiscout.inet.tu-berlin.de/api/v1/device/?key=" .. key .. "&signature=" .. data2.signature
-							local rc, r, txt, s = httpclient.request_raw(url)
-							local json = require "luci.json"
-							local req = json.decode(txt)
-							if not(req["results"] == nil) then
-								devicename = req["results"][1]["name"]
-							end
-						end
+					for clientmac, clientvals in pairs(data) do
+					if (type(clientvals) == "table") then 
 					%>
+						<ul>
 						<li>
-						<strong>Client: <%= mac2 %><br />
-                                              	<strong>Name:</strong> <%= host.name %><br />
-						<strong>IPv4:</strong> <%= host.ipv4 %><br />
-						<strong>IPv6:</strong> <%= host.ipv6 %><br /> 
-			                        <strong>Frequency is: </strong><%= "%.3f" %( data2.freq / 1000 ) %> GHz (Channel: <%= "%d" %( status.frequency_to_channel(data2.freq) ) %>)<br />
-						<strong>HT: </strong><%= (data2.ht == true) and "available" or "not available" %><br />
-						<strong>VHT: </strong><%= (data2.vht == true) and "available" or "not available" %><br />
-						<strong>Signature: </strong><%= "%s" %data2.signature %><br />
-						<strong>Devicename: </strong><%= "%s" %devicename %><br />
-						<!--
-						<strong>Signal is: </strong><%= "%d" %data2.signal %><br />
-						<strong>Channel Utilization is: </strong><%= "%d" %data2.channel_utilization %><br />
-						<strong>Station connected to AP is: </strong><%= "%d" %data2.num_sta %><br />
-						--!>
+						<strong>MAC: </strong><%= clientmac %><br />
+						<strong>HT: </strong><%= (clientvals.ht == true) and "available" or "not available" %><br />
+						<strong>VHT: </strong><%= (clientvals.vht == true) and "available" or "not available" %><br />
+						<strong>Signal is: </strong><%= "%d" %clientvals.signal %><br />
 						</li>
+						</ul>
 					<%
 					end
+					end
 					%>
-				</ul>
 				<%
 				end
 				%>
