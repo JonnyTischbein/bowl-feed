e0d42a3929d367f6e17b64a0ee46a08538def239
Jonny Tischbein <jonny_tischbein@systemli.org>
2020-03-24 23:50:10 +0100

    rework network overview with tables

--- luci-app-dawn/luasrc/model/cbi/dawn/dawn_network.lua
+++ luci-app-dawn/luasrc/model/cbi/dawn/dawn_network.lua
@@ -8,58 +8,84 @@ function s.render(self, sid)
 	local json = require "luci.json"
 	local utl = require "luci.util"
 	tpl.render_string([[
-		<ul>
+		<table class="table" style="border: 1px solid grey;">
+			<thead style="background-color: grey; color: white;">
+				<tr>
+						<th>SSID</th>
+						<th>MAC</th>
+						<th>Channel Utilization</th>
+						<th>Frequency</th>
+						<th>Stations</th>
+						<th>HT Sup</th>
+						<th>VHT Sup</th>
+				</tr>
+			</thead>
+			<tbody>
 			<%
-            		local status = require "luci.tools.ieee80211"
+			local status = require "luci.tools.ieee80211"
 			local utl = require "luci.util"
-		        local sys = require "luci.sys"     
-		        local hosts = sys.net.host_hints() 
+			local sys = require "luci.sys"
+			local hosts = sys.net.host_hints()
 			local stat = utl.ubus("dawn", "get_network", { })
 			local name, macs
 			for name, macs in pairs(stat) do
-			%>
-				<li>
-					<strong>SSID is: </strong><%= name %><br />
-				</li>
-				<ul>
-				<%
 				local mac, data
 				for mac, data in pairs(macs) do
-				%>
-					<li>
-						<strong>MAC: </strong><%= mac %><br />
-						<strong>Channel Utilization: </strong><%= "%d" %data.channel_utilization %><br />
-						<strong>Frequency is: </strong><%= "%.3f" %( data.freq / 1000 ) %> GHz (Channel: <%= "%d" %( status.frequency_to_channel(data.freq) ) %>)<br />
-						<strong>Stations: </strong><%= "%d" %data.num_sta %><br />
-						<strong>HT Sup: </strong><%= (data.ht_support == true) and "available" or "not available" %><br />
-						<strong>VHT Sup: </strong><%= (data.vht_support == true) and "available" or "not available" %><br />  
-						<strong>Clients: </strong><br />	
-					</li>
-                                        <% 					
+			%>
+				<tr class="center">
+					<td><%= name %></td>
+					<td><%= mac %></td>
+					<td><%= "%.2f" %(data.channel_utilization / 2.55) %> %</td>
+					<td><%= "%.3f" %( data.freq / 1000 ) %> GHz (Channel: <%= "%d" %( status.frequency_to_channel(data.freq) ) %>)</td>
+					<td><%= "%d" %data.num_sta %></td>
+					<td><%= (data.ht_support == true) and "available" or "not available" %></td>
+					<td><%= (data.vht_support == true) and "available" or "not available" %></td>
+				</tr>
+				<tr>
+					<td colspan="7"><hr></td>
+				</tr>
+				<tr>
+					<td colspan="2" class="center"><strong>Clients</strong></td>
+					<td colspan="4">
+					<table class="table" style="border: 1px solid grey;">
+					<thead style="background-color: grey; color: white;">
+						<tr>
+							<th>MAC</th>
+							<th>HT</th>
+							<th>VHT</th>
+							<th>Signal</th>
+						</tr>
+					</thead>
+					<tbody>
+					<%
 					local mac2, data2
 					for clientmac, clientvals in pairs(data) do
-					if (type(clientvals) == "table") then 
+						if (type(clientvals) == "table") then 
 					%>
-						<ul>
-						<li>
-						<strong>MAC: </strong><%= clientmac %><br />
-						<strong>HT: </strong><%= (clientvals.ht == true) and "available" or "not available" %><br />
-						<strong>VHT: </strong><%= (clientvals.vht == true) and "available" or "not available" %><br />
-						<strong>Signal is: </strong><%= "%d" %clientvals.signal %><br />
-						</li>
-						</ul>
+						<tr class="center">
+							<td><%= clientmac %></td>
+							<td><%= (clientvals.ht == true) and "available" or "not available" %></td>
+							<td><%= (clientvals.vht == true) and "available" or "not available" %></td>
+							<td><%= "%d" %clientvals.signal %></td>
+						</tr>
 					<%
-					end
+						end
 					end
 					%>
+					</tbody>
+					</table>
+				</tr>
+				<tr>
+					<td colspan="7"><hr></td>
+				</tr>
 				<%
 				end
 				%>
-			</ul>
 			<%
 			end
 			%>
-		</ul>
+			</tbody>
+		</table>
 	]])
 end
 
